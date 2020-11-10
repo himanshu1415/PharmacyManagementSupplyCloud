25795e0bacc9f8d5f350f81c141dcdcee1ec8061
ArhanKhan98 <39579407+ArhanKhan98@users.noreply.github.com>
2020-11-10 13:45:36 +0530

    local host or cloud

--- PharmacyMedicineSupplyApi/Respository/SupplyRepository.cs
+++ PharmacyMedicineSupplyApi/Respository/SupplyRepository.cs
@@ -22,7 +22,7 @@ namespace PharmacyMedicineSupplyApi.Respository
             {
                 using (var httpclient = new HttpClient())
                 {
-                    httpclient.BaseAddress = new Uri("https://localhost:44366/");
+                    httpclient.BaseAddress = new Uri("52.224.195.82");
                     HttpResponseMessage res = await httpclient.GetAsync("MedicineStockInformation");
                     _log4net.Info("GetSupplies request Initiated for the Medicine Api");
                     if (res.IsSuccessStatusCode)
